d43166f4b6c0bd10894ec36940d0451761837e5d
Rinlapat Pu <prair@rinlapat.me>
2015-05-24 13:52:10 +0700

    Change input variables in problem 3.8 to 'Hour', 'EXITSn_hourly', 'fog', 'rain', and ’thunder’.

--- p1_analyzing_nyc_subway/ps3_analyzing_subway_data/problem8.py
+++ p1_analyzing_nyc_subway/ps3_analyzing_subway_data/problem8.py
@@ -7,7 +7,7 @@ import statsmodels.api as sm
 
 def predictions(weather_turnstile):
     y = weather_turnstile['ENTRIESn_hourly']
-    x = weather_turnstile[['Hour', 'EXITSn_hourly', 'maxpressurei', 'maxdewpti', 'mindewpti', 'minpressurei', 'meandewpti', 'meanpressurei', 'fog', 'rain', 'meanwindspdi', 'mintempi', 'meantempi', 'maxtempi', 'precipi', 'thunder']]
+    x = weather_turnstile[['Hour', 'EXITSn_hourly', 'fog', 'rain', 'thunder']]
     x = sm.add_constant(x)
     model = sm.OLS(y, x)
     model_fit = model.fit()

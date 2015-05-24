e85c64a64690c01cc8d44ee35696fc85340403f8
Rinlapat Pu <prair@rinlapat.me>
2015-05-24 14:50:24 +0700

    Modify problem 3.8

--- p1_analyzing_nyc_subway/ps3_analyzing_subway_data/problem8.py
+++ p1_analyzing_nyc_subway/ps3_analyzing_subway_data/problem8.py
@@ -7,10 +7,9 @@ import statsmodels.api as sm
 
 def predictions(weather_turnstile):
     y = weather_turnstile['ENTRIESn_hourly']
-    x = weather_turnstile[['Hour', 'EXITSn_hourly', 'fog', 'rain', 'thunder']]
+    x = weather_turnstile[['rain', 'Hour', 'EXITSn_hourly']]
     x = sm.add_constant(x)
     model = sm.OLS(y, x)
     model_fit = model.fit()
-    prediction = model_fit.predict()
-    
-    return prediction
+
+    return model_fit.predict()
